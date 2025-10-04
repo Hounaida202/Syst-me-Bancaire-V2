0b6da9507cb779b820cf89e88e7261474c29ef11
Hounaida202 <etrichih@gmail.com>
2025-10-04 23:42:48 +0100

    la'jout des deux fonctionnalités  , de l ajout d un compte epargne , et de obtenir le compte avec min solde

--- tt/src/DAO/ClientDAO.java
+++ tt/src/DAO/ClientDAO.java
@@ -36,6 +36,59 @@ public class CompteDAO {
         }
     }
 
+    public static void addEpargneAccount(CompteEpargne compte) {
+
+        String sql = "INSERT INTO comptes VALUES (?, ?, ?, ?, ? ,?)";
+        try {
+            Connection conn = Database.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setObject(1, compte.getId_compte());
+            stmt.setString(2, compte.getNumero());
+            stmt.setDouble(3, compte.getSolde());
+            stmt.setObject(4, compte.getId_client());
+            stmt.setObject(5,null);
+            stmt.setDouble(6, compte.getTaux());
+            stmt.executeUpdate();
+            System.out.println("CompteEpargne " + compte.getId_client()+ " ajouté avec succés");
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static Compte getminimumSolde(){
+        String sql ="SELECT * FROM comptes ORDER BY solde ASC LIMIT 1";
+        try {
+            Connection conn = Database.getConnection();
+
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            if(rs.next()){
+                String id = rs.getString("id_compte");
+                String numero = rs.getString("numero");
+                double solde = rs.getDouble("solde");
+                String id_client = rs.getString("id_client");
+                Double decouvert = rs.getDouble("decouvert");
+                Double taux = rs.getDouble("taux");
+                if(decouvert == null){
+                    Compte compte = new CompteEpargne(id,solde,id_client,taux);
+                }
+                else {
+                    Compte compte = new CompteCourant(id,solde,id_client,decouvert);
+                }
+            }
+            else {
+                return null;
+            }
+
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
 
 
 }
