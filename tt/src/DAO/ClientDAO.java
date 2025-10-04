c12fa0a0b5db25a90b65d5db07cac56b03807c1f
Hounaida202 <etrichih@gmail.com>
2025-10-04 23:48:11 +0100

    la methode de l ajout d un client

--- tt/src/DAO/ClientDAO.java
+++ tt/src/DAO/ClientDAO.java
@@ -16,6 +16,19 @@ import java.util.List;
 
 public class ClientDAO {
 
+    public static void addClient(Client client) {
+        String sql = "INSERT INTO Clients (id_client, nom, email) VALUES (?, ?, ?)";
+        try (Connection conn = Database.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, client.id_client());
+            stmt.setString(2, client.nom());
+            stmt.setString(3, client.email());
+            stmt.executeUpdate();
+            System.out.println("Client " + client.id_client()+ " ajouté avec succés");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 
 }
